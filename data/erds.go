041f323129b2b3198dac7ea0bc52bc065f8004ec
Andrew Mathies <andrewmathies@yahoo.com>
2020-02-29 15:31:51 -0500

    forgot to re-track erds data module

--- data/erds.go
+++ data/erds.go
@@ -1,17 +1,44 @@
 package data
 
-import "net/http"
+import (
+	"encoding/json"
+	"io"
+	"time"
+)
 
 type ERD struct {
-	name    string
-	address uint16
-	fields  []string
-	actions []string
+	ID        int      `json:"id"`
+	Name      string   `json:"name"`
+	Address   string   `json:"address"`
+	Fields    []string `json:"fields"`
+	Actions   []string `json:"actions"`
+	CreatedOn string   `json:"-"`
+	UpdatedOn string   `json:"-"`
+	DeletedOn string   `json:"-"`
 }
 
-type ERDs *[]ERD
+type ERDs []*ERD
 
-var erds *[]ERD
+var erds = []*ERD{
+	&ERD{
+		ID:        0,
+		Name:      "ERD_ApplianceType",
+		Address:   "0x0090",
+		Fields:    []string{"ApplianceType"},
+		Actions:   []string{"Read", "Subscribe"},
+		CreatedOn: time.Now().UTC().String(),
+		UpdatedOn: time.Now().UTC().String(),
+	},
+	&ERD{
+		ID:        1,
+		Name:      "ERD_OvenInfo",
+		Address:   "0x5007",
+		Fields:    []string{"OvenInfo"},
+		Actions:   []string{"Read", "Write", "Subscribe", "Publish"},
+		CreatedOn: time.Now().UTC().String(),
+		UpdatedOn: time.Now().UTC().String(),
+	},
+}
 
 func GetERDs() ERDs {
 	return erds
@@ -22,6 +49,7 @@ func LoadERDs(path string) {
 	return
 }
 */
-func ToJSON(rw http.ResponseWriter) error {
-
+func (e *ERDs) ToJSON(w io.Writer) error {
+	enc := json.NewEncoder(w)
+	return enc.Encode(e)
 }
