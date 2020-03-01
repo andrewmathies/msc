f45ca6d09ce8e0030da564311e4662dfa85dd514
Andrew Mathies <andrewmathies@yahoo.com>
2020-03-01 15:47:00 -0500

    dont need bash script testing files in repo

--- post_req.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-curl localhost:9090/erds -d '{"name": "Erd_CameraStatus", "address":"0x0503", "fields": ["Erd_CameraStatus"], "actions": ["Read", "Write"] }'
--- put_req.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-curl -v localhost:9090/erds/2 -XPUT -d '{"name": "Erd_CameraStatusNew", "address":"0x0504", "fields": ["Erd_CameraStatusNew"], "actions": ["Read", "Write"] }'
